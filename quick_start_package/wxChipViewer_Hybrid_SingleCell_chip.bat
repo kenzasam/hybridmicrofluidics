c8dfcef9bc90c1a647e0756f313f72b30161a2c1
Kenza Samlali <kenzasamlali@gmail.com>
2019-10-16 21:49:34 +0000

    wxChipViewer_Hybrid_SingleCell_chip.bat edited online with Bitbucket

--- quick_start_package/wxChipViewer_Hybrid_SingleCell_chip.bat
+++ quick_start_package/wxChipViewer_Hybrid_SingleCell_chip.bat
@@ -1,2 +1,2 @@
-rem E:\GuySoftware\UdpTo_wxChipViewer\UdpTo_wxChipViewer.exe -f Kenza_wizzardv4.cfg -w 840 -v 450 -u 7010
-E:\GuySoftware\UdpTo_wxChipViewer\UdpTo_wxChipViewer.exe -f Kenza_wizzardv4.cfg -w 840 -v 450 -u 7010
+rem E:\GuySoftware\UdpTo_wxChipViewer\UdpTo_wxChipViewer.exe -f Hybrid_SingleCell_chip.cfg.cfg -w 840 -v 450 -u 7010
+E:\GuySoftware\UdpTo_wxChipViewer\UdpTo_wxChipViewer.exe -f Hybrid_SingleCell_chip.cfg.cfg -w 840 -v 450 -u 7010
